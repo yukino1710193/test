cd3ef33e83dcac874ae931935ef22cc5f0cc72da
yukino1710193 <sasukevp1959@gmail.com>
2024-05-24 17:01:07 +0700

    24/5

--- k8sInitCluster.sh
+++ k8sInitCluster.sh
@@ -8,12 +8,10 @@
     sudo rm -rf $HOME/.kube/config
 
 # Giờ thì tạo cluster nào ( đã config trước environment rồi )
-sudo kubeadm init --control-plane-endpoint=master-node --upload-certs --apiserver-bind-port=8080 --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --control-plane-endpoint=master-node --upload-certs --apiserver-bind-port=8080 --pod-network-cidr=192.168.0.0/16
 
     mkdir -p $HOME/.kube
     echo y | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Create Network pod using flannel
-    kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
-    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+# Create Network pod using Calico
