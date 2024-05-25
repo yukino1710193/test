5877818a7972ccb83211b589f77f094d95c312aa
yukino1710193 <sasukevp1959@gmail.com>
2024-05-25 14:29:19 +0700

    25/5

---
+++ set_master_node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+    echo Đây là Yukino , tôi sẽ thực hiện set env cho master-node để chuẩn bị tạo Cluster
+
+
+# Install Docker - phần này được cài rồi nên sẽ để ghi chú , nếu chưa có thì có thể CTRL + K + U 
+    # sudo apt update
+    # sudo apt install docker.io -y
+    # sudo systemctl enable 
+    # sudo systemctl status docker
+    # sudo systemctl start docker
+# Install Kubernetes
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo apt update
+# Install kubeadm , kubelet , kubectl
+    sudo apt install kubeadm kubelet kubectl
+    sudo apt-mark hold kubeadm kubelet kubectl # hold version 
+#
