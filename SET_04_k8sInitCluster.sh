cc9d93dcab46d3fc40aab8ad14a87e37ba9ca985
yukino1710193 <sasukevp1959@gmail.com>
2024-05-25 18:24:04 +0700

    25/5

--- SET_04_k8sInitCluster.sh
+++ SET_04_k8sInitCluster.sh
@@ -8,7 +8,7 @@
     sudo rm -rf $HOME/.kube/config
 
 # Giờ thì tạo cluster nào ( đã config trước environment rồi )
-sudo kubeadm init --apiserver-advertise-address=192.168.101.126 --control-plane-endpoint=master-node --upload-certs --apiserver-bind-port=8080 --pod-network-cidr=192.128.0.0/16
+sudo kubeadm init --apiserver-advertise-address=192.168.101.126 --control-plane-endpoint=masternode --upload-certs --apiserver-bind-port=8080 --pod-network-cidr=192.128.0.0/16
 
     mkdir -p $HOME/.kube
     echo y | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
