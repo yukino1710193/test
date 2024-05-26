5337679a8d9546b1f9ae490ad81e1ddc1e58a021
yukino1710193 <sasukevp1959@gmail.com>
2024-05-26 14:44:58 +0700

    26/5

--- k8s_setting_up/SET_05_Network_pod.sh
+++ k8s_setting_up/SET_05_Network_pod.sh
@@ -3,4 +3,19 @@
 
 # Create Network pod using Calico
     kubectl apply -f ~/Documents/test/kube-flannel.yml
-    
+    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+    sudo ifconfig flannel.1 down
+    sudo ifconfig flannel.1 up
+
+# fix coreDNS pending 
+# sudo nano /var/lib/kubelet/config.yaml
+# evictionHard:
+#   imagefs.available: 1%
+#   memory.available: 100Mi
+#   nodefs.available: 1%
+#   nodefs.inodesFree: 1%
+# sudo systemctl restart kubelet
+#
+# kubectl taint nodes <node-name> node.kubernetes.io/not-ready-
+# kubectl apply -f ~/Documents/test/kube-flannel.yml
+# kubectl delete pod -n kube-system coredns ...
